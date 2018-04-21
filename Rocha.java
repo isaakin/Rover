151d89c731d5d55884253593abc77ae8dc0202f7
Isaac Augusto <38545209+isaakin@users.noreply.github.com>
2018-04-21 19:45:30 -0300

    Add files via upload

---
+++ Rocha.java
@@ -0,0 +1,13 @@
+public class Rocha{
+private String tipo;
+private SubstanciaQuimica substancia[];
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+
+}
